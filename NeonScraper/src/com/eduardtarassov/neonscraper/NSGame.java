52ee0868281beaac9d41fb379ec1827d9e2fe947
Eduard Tarassov <eduardtarassov@gmail.com>
2014-03-10 06:08:56 +0000

    Set application to white screen. Works both on android and desktop.

--- NeonScraper/src/com/eduardtarassov/neonscraper/NSGame.java
+++ NeonScraper/src/com/eduardtarassov/neonscraper/NSGame.java
@@ -1,6 +1,7 @@
 package com.eduardtarassov.neonscraper;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,8 +10,10 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.eduardtarassov.nshelpers.AssetLoader;
+import com.eduardtarassov.screens.GameScreen;
 
-public class NSGame implements ApplicationListener {
+public class NSGame extends Game {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
@@ -18,21 +21,9 @@ public class NSGame implements ApplicationListener {
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		System.out.println("Game Created!");
+        AssetLoader.load();
+        setScreen(new GameScreen());
 	}
 
 	@Override
@@ -46,10 +37,10 @@ public class NSGame implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
+		/*batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
-		batch.end();
+		batch.end(); */
 	}
 
 	@Override
