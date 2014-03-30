adc8b3c78e711796ad303d37e6dab3fe2e8fa437
Eduard Tarassov <eduardtarassov@gmail.com>
2014-03-30 14:44:05 +0100

    Class Grid is mostly done.

--- NeonScraper/src/com/eduardtarassov/gameobjects/Grid.java
+++ NeonScraper/src/com/eduardtarassov/gameobjects/Grid.java
@@ -11,59 +11,59 @@ import java.util.Random;
 public class Grid {
     private static GridUnit[][] gridUnits;
     public static Grid instance;
-    private static int gridLeft;
-    private static int gridTop;
-    private static int gridBottom;
-    private static int gridRight;
     private static int gridDividedColumns;
-    private static int gridDividedRows
+    private static int gridDividedRows;
 
+            private static int gridWidth;
+    private static int gridHeight;
     private static final int GRID_COLUMNS = 12;
     private static final int GRID_ROWS = 12;
 
 
     public static void setInstance(int playAreaLeft, int playAreaTop, int playAreaRight, int playAreaBottom) {
-        gridLeft = playAreaLeft;
-        gridRight = playAreaRight;
-        gridTop = playAreaTop;
-        gridBottom = playAreaBottom;
 
         //gridColumns=(Math.abs(playAreaRight-playAreaLeft)/GRID_WIDTH);
         //gridRows=(Math.abs(playAreaBottom-playAreaTop)/GRID_HEIGHT);
-       gridDividedColumns =  (gridRight - gridLeft) / GRID_COLUMNS;
-       gridDividedRows = (gridBottom - gridTop) / GRID_ROWS;
+        gridWidth = playAreaRight - playAreaLeft;
+        gridHeight = playAreaBottom - playAreaTop;
+       gridDividedColumns = gridWidth / GRID_COLUMNS;
+       gridDividedRows = gridHeight / GRID_ROWS;
 
         gridUnits = new GridUnit[GRID_COLUMNS][GRID_ROWS];
         for (int i = 0; i < GRID_COLUMNS; i++) {
             for (int j = 0; j < GRID_ROWS; j++) {
 
-                gridUnits[i][j] = new GridUnit(gridRight - gridLeft, gridBottom - gridTop, i*gridDividedColumns, j*gridDividedRows, false);
+                gridUnits[i][j] = new GridUnit(gridWidth, gridHeight, i*gridDividedColumns, j*gridDividedRows, false);
             }
 
         }
 
     }
 
-    public static void setObjectAt(float x, float y, boolean type) {
+    public static void setObjectAt(float x, float y) {
         int yindex = (int) x / gridDividedColumns;
         int xindex = (int) y / gridDividedRows;
-        gridUnits[xindex][yindex].objectType = type;
+         if (!gridUnits[xindex][yindex].objectType)
+        gridUnits[xindex][yindex].objectType = true;
     }
 
     public static void RemoveObjectAt(int x, int y) {
-        setObjectAt(x, y, false);
+        int yindex = (int) x / gridDividedColumns;
+        int xindex = (int) y / gridDividedRows;
+        if (gridUnits[xindex][yindex].objectType)
+            gridUnits[xindex][yindex].objectType = false;
     }
 
-    public static Vector2 GetRandomIndex() {
+    public static Vector2 setRandomIndex() {
         Random r = new Random();
         while (true) {
-            int y = r.nextInt(gridColumns);
-            int x = r.nextInt(gridRows);
-            if (!gridUnits[x][y].objectType)
-                return new Vector2(x * GRID_WIDTH + gridLeft, y * GRID_HEIGHT + gridTop);
+            int x = r.nextInt(gridWidth + 1);
+            int y = r.nextInt(gridHeight + 1);
+            setObjectAt(x, y);
         }
     }
 
+    //!!!!!!!!!!!!!!
     public static boolean getCellEmpty(int gx, int gy) {
         return gridUnits[gx][gy].objectType;
     }
