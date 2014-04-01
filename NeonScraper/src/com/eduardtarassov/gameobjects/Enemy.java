78835acf35d39464e5ee1838fa90b5410bd9571d
Eduard Tarassov <eduardtarassov@gmail.com>
2014-04-01 13:16:45 +0100

    For now created ArrayList of behaviours with the function to add behaviours and remove them. Next step is to create a seeker enemy and iterate with it.

--- NeonScraper/src/com/eduardtarassov/gameobjects/Enemy.java
+++ NeonScraper/src/com/eduardtarassov/gameobjects/Enemy.java
@@ -4,11 +4,17 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.Color;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Eduard on 28/03/14.
  */
 public class Enemy extends Entity {
     private int timeUntilStart = 60;
+
+    // We  will be adding Iterable<Enemy> methods with enemy behaviour into our Iterable<Enemy> type ArrayList
+    private ArrayList<Iterator <Enemy>> behaviours = new ArrayList();
 
     public boolean isActive() {
         return timeUntilStart <= 0;
@@ -19,12 +25,16 @@ public class Enemy extends Entity {
         this.position = position;
         radius = image.getRegionWidth() / 2.0f;
         color = Color.CLEAR; //!!!!! I am not sure this is gonna be transparent one.
+
     }
 
     @Override
     public void update() {
         if (timeUntilStart <= 0) {
-            //enemy behaviour logic goes here.
+            // Enemy behaviour logic goes here.
+             if (timeUntilStart <= 0){
+                 applyBehaviours();
+             }
         } else {
             timeUntilStart--;
             //color = Color.WHITE * (1 - timeUntilStart / 60f);
@@ -40,16 +50,33 @@ public class Enemy extends Entity {
         isExpired = true;
     }
 
-    protected void followPlayer() {
+           /*
+           * This method will add the different behaviour methods into our Arraylist behaviours
+           */
+    private void addBehaviour(Iterable<Enemy> behaviour){
+        behaviours.add(behaviour.iterator());
+    }
+
+    private void applyBehaviours(){
+        for (int i = 0; i < behaviours.size(); i++){
+            if (behaviours.get(i).next() == null){
+                   behaviours.remove(i--);
+            }
+        }
+    }
+
+    private Iterable<Enemy> followPlayer() {
         while (true) {
             velocity.add(PlayerShip.instance.position.sub(position).scl(acceleration));
             if (velocity != Vector2.Zero)
                 orientation = velocity.angle();
+
             Thread.yield();
+
         }
     }
 
-    protected void moveInASquare(){
+    protected Iterable<Enemy> moveInASquare(){
         final int framesPerSide = 30;
         while (true){
             // Move right for 30 frames
